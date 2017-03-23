3a4975eed5264ea168a0a716d8d2560d9827a7dd
Andrew Heiko <geiko2015@gmail.com>
2017-03-23 09:08:01 +0200

    Add database config

---
+++ src/main/java/com/geiko/configs/DatabaseConfig.java
@@ -0,0 +1,63 @@
+package com.geiko.configs;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Created by Андрей on 22.03.2017.
+ */
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+    @Value("${db.driver}")
+    private String DB_DRIVER;
+    @Value("${db.password}")
+    private String DB_PASSWORD;
+    @Value("${db.url}")
+    private String DB_URL;
+    @Value("${db.username}")
+    private String DB_USERNAME;
+    @Value("${hibernate.dialect}")
+    private String HIBERNATE_DIALECT;
+    @Value("${entitymanager.packagesToScan}")
+    private String ENTITYMANAGER_PACKAGES_TO_SCAN;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(DB_DRIVER);
+        dataSource.setUrl(DB_URL);
+        dataSource.setUsername(DB_USERNAME);
+        dataSource.setPassword(DB_PASSWORD);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan(ENTITYMANAGER_PACKAGES_TO_SCAN);
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", HIBERNATE_DIALECT);
+        sessionFactoryBean.setHibernateProperties(hibernateProperties);
+        return sessionFactoryBean;
+    }
+
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+        return transactionManager;
+    }
+
+}
